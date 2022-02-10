481598a87a46ba32773a439fda6f87dc68fda063
Enoch Tan <enochtan@umich.edu>
2022-02-10 18:48:37 -0500

    ssl modified

--- config/database.js
+++ config/database.js
@@ -15,7 +15,10 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
-      "ssl": true
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
     },
     seederStorage: 'sequelize',
   }
